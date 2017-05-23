e428ef05e940d7da18e2a5dd349cf8b4d2ddd8fd
Luis Eduardo Martin <lemartinp@unal.edu.co>
2017-05-23 14:39:19 -0500

    first commit bruh
    added frequencies to some other signals, 12 more to go

--- Imagen.m
+++ Imagen.m
@@ -19,7 +19,7 @@ hold on;
 plot(tss,F1,'b')
 %}
 
-[X,map]  = imread('Color wheel.png');
+[X,map]  = imread('g.jpg');
 %image(X);
 A = double(X);
 disp(length(A))
@@ -54,7 +54,7 @@ while i < length(R)
                 txt = [txt, 'kinda blue'];
             else
                 txt = [txt, 'not so blue'];
-                signal = addSignal(659.25,ts,signal);
+                signal = addSignal(466,ts,585,signal);
             end
         elseif (green < 191) && (green > 63)
             txt = [txt, 'kinda green'];
@@ -64,19 +64,19 @@ while i < length(R)
                 txt = [txt, 'kinda blue'];
             else
                 txt = [txt, 'not so blue'];
-                signal = addSignal(622.25,ts,signal);
+                signal = addSignal(440,ts,620,signal);
             end
         else
             txt = [txt, 'not so green'];
             if (blue > 191)
                 txt = [txt, 'blueish'];
-                signal = addSignal(523.25,ts,signal);
+                signal = addSignal(523.25,ts,1,signal);
             elseif (blue < 191) && (blue > 63)
                 txt = [txt, 'kinda blue'];
-                signal = addSignal(554.37,ts,signal);
+                signal = addSignal(554.37,ts,1,signal);
             else
                 txt = [txt, 'not so blue'];
-                signal = addSignal(587.33,ts,signal);
+                signal = addSignal(415,ts,657,signal);
             end
         end
     elseif (red < 191) && (red > 63)
@@ -89,7 +89,7 @@ while i < length(R)
                 txt = [txt, 'kinda blue'];
             else
                 txt = [txt, 'not so blue'];
-                signal = addSignal(689.46,ts,signal);
+                signal = addSignal(494,ts,552,signal);
             end
         elseif (green < 191) && (green > 63)
             txt = [txt, 'kinda green'];
@@ -104,11 +104,13 @@ while i < length(R)
             txt = [txt, 'not so green'];
             if (blue > 191)
                 txt = [txt, 'blueish'];
-                signal = addSignal(493.88,ts,signal);
+                signal = addSignal(659,ts,414,signal);
             elseif (blue < 191) && (blue > 63)
                 txt = [txt, 'kinda blue'];
+                signal = addSignal(698,ts,390,signal);
             else
                 txt = [txt, 'not so blue'];
+                signal = addSignal(370,ts,737,signal);
             end
         end
     else
@@ -117,19 +119,19 @@ while i < length(R)
             txt = [txt, 'greenish'];
             if (blue > 191)
                 txt = [txt, 'blueish'];
-                signal = addSignal(830.61,ts,signal);
+                signal = addSignal(554,ts,492,signal);
             elseif (blue < 191) && (blue > 63)
                 txt = [txt, 'kinda blue'];
-                signal = addSignal(783.99,ts,signal);
+                signal = addSignal(5,ts,1,signal);
             else
                 txt = [txt, 'not so blue'];
-                signal = addSignal(739.99,ts,signal);
+                signal = addSignal(523,ts,521,signal);
             end
         elseif (green < 191) && (green > 63)
             txt = [txt, 'kinda green'];
             if (blue > 191)
                 txt = [txt, 'blueish'];
-                signal = addSignal(440.00,ts,signal);
+                signal = addSignal(587,ts,464,signal);
             elseif (blue < 191) && (blue > 63)
                 txt = [txt, 'kinda blue'];
             else
@@ -139,7 +141,7 @@ while i < length(R)
             txt = [txt, 'not so green'];
             if (blue > 191)
                 txt = [txt, 'blueish'];
-                signal = addSignal(466.16,ts,signal);
+                signal = addSignal(622,ts,438,signal);
             elseif (blue < 191) && (blue > 63)
                 txt = [txt, 'kinda blue'];
             else
@@ -155,6 +157,13 @@ tss(length(tss)-1) =[];
 subplot(2,1,2);
 plot(tss, signal, 'r');
 %sound(signal);
+fileID = fopen('log.txt', 'w');
+formatSpec = '%s \n';
+for e = length(txt)
+    fprintf(fileID, formatSpec, txt(e));
+end
+fclose(fileID);
+type log.txt
 
 %{
 Tam=size(B);
--- addSignal.m
+++ addSignal.m
@@ -1,5 +1,5 @@
-function signal = addSignal(f1,ts,signal)
-    F1 = sin(f1*ts*2*pi);
+function signal = addSignal(f1,ts,wl,signal)
+    F1 = wl*sin(f1*ts*2*pi);
     signal = [signal, F1];
     disp('señal agregada')
 end
