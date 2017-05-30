da533d1af7e54e7de82187a252df1b21359ddc81
Anni Piragauta <anni.apu@gmail.com>
2017-05-30 13:22:43 -0500

    modify singulazer to dont convert every single pixel or the image

--- test.m
+++ test.m
@@ -1,17 +1,25 @@
 clear;
 clc;
 
-image  = imread('src/img/Dogo.jpg');
-A = double(image);
+% [y,fs] = audioread('src/chords/major C#.wav');
+% [y1] = audioread('src/chords/major A.wav');
+% signal = [y(1:100000,:); y1(1:100000,:)];
+% 
+% plot(signal);
+% sound(signal,32768);
+% grid;
 
-cols = length(A);
-step = round(cols/60);
-if cols > 60
-    R = mean(A(:,(1:step:cols),1));
-    G = mean(A(:,(1:step:cols),2));
-    B = mean(A(:,(1:step:cols),3));
-else
-    R = mean(A(:,:,1));
-    G = mean(A(:,:,2));
-    B = mean(A(:,:,3));
-end
+image  = imread('src/img/Dogo.jpg');
+% A = double(image);
+
+signal = signalizer(image);
+
+% rows = length(A(:,1,1));
+% cols = length(A);
+% step = 60;
+% 
+% W = mean(A(1:step:rows,1:step:cols,:));
+% 
+% R = mean(A(1:rows,1:cols,1));
+% G = mean(A(1:rows,1:cols,2));
+% B = mean(A(1:rows,1:cols,3));
