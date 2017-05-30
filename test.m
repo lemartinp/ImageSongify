0b9f8496d4e5292d3b148448ebf94bd7c79c877b
Anni Piragauta <anni.apu@gmail.com>
2017-05-30 08:33:10 -0500

    stable with chords

--- test.m
+++ test.m
@@ -3,18 +3,15 @@ clc;
 
 image  = imread('src/img/Dogo.jpg');
 A = double(image);
-M = mean(double(image));
 
-R = mean(A(:,(1:round(length(M)/60):length(M)),1));
-G = mean(A(:,(1:round(length(M)/60):length(M)),2));
-B = mean(A(:,(1:round(length(M)/60):length(M)),3));
-
-i = round(length(R)/60);
-n = 1;
-k = i;
-while k < length(R)
-    av_red = mean(M(:,n:k,1));
-    %C(index,null,null) = av_red;
-    n=n+i;
-    k=k+i;
+cols = length(A);
+step = round(cols/60);
+if cols > 60
+    R = mean(A(:,(1:step:cols),1));
+    G = mean(A(:,(1:step:cols),2));
+    B = mean(A(:,(1:step:cols),3));
+else
+    R = mean(A(:,:,1));
+    G = mean(A(:,:,2));
+    B = mean(A(:,:,3));
 end
